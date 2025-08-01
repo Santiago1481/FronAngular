70cd5c990a660ccfa72ffd4f8524322179e6914d
jesus david fierro rivera <jesusdavidfierrorivera817@gmail.com>
2025-08-01 11:51:37 -0500

    Estructura del proyecto

---
+++ Front/src/app/features/podio/podio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-podio',
+  imports: [],
+  templateUrl: './podio.component.html',
+  styleUrl: './podio.component.css'
+})
+export class PodioComponent {
+
+}
---
+++ Front/src/app/features/sala-espera/sala-espera.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sala-espera',
+  imports: [],
+  templateUrl: './sala-espera.component.html',
+  styleUrl: './sala-espera.component.css'
+})
+export class SalaEsperaComponent {
+
+}
