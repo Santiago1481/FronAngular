9524c0ac067a22535bdc08715ad56434f8ee52ca
JesusDavid1405 <jesusdavidfierrorivera817@gmail.com>
2025-08-02 22:15:05 -0500

    estructura de podio

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { InicioComponent } from './features/inicio/inicio.component';
 import { SalaEsperaComponent } from './features/sala-espera/sala-espera.component';
 import { CartaComponent } from './shared/components/carta/carta.component';
 import { JuegoComponent } from './features/juego/juego.component';
+import { PodioComponent } from './features/podio/podio.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/inicio', pathMatch: 'full' },
     { path: 'inicio', component: InicioComponent },
     { path: 'sala', component: SalaEsperaComponent },
-    {path: 'juego',component: JuegoComponent},
-
-    {path:'card',component:CartaComponent}
+    { path: 'juego',component: JuegoComponent},
+    { path: 'podio', component: PodioComponent },
+    { path:'card',component:CartaComponent}
 ];
