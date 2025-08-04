464ae1e4a5cab306d49bd514fdaa0379ad36854f
CristianJPalma <cristianspalmas@gmail.com>
2025-08-04 11:11:35 -0500

    id carta

--- Front/src/app/shared/components/carta/carta.component.ts
+++ Front/src/app/shared/components/carta/carta.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, Input, OnInit } from '@angular/core';
 import { CardModel } from '../../Models/card.model';
 import { CommonModule } from '@angular/common';
+import { CartaService } from '../../../services/carta/carta.service';
 
 @Component({
   selector: 'app-carta',
@@ -9,10 +10,18 @@ import { CommonModule } from '@angular/common';
   styleUrl: './carta.component.css'
 })
 export class CartaComponent{
-
-
+  
   @Input() card!: CardModel;
+  cardServie = inject(CartaService);
 
+  cardId?: CardModel;
+  
 
+  getAttributeValue(id:number) {
+    return this.cardServie.getCardById(id).subscribe((data)=>{
+      this.cardId = data;
+      console.log(this.cardId);
+    })
 
+  }
 }
